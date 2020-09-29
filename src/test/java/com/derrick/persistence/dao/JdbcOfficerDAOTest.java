65a384007eb6560dee6ffeffe24f85df81af3971
derrick.njeru <dderrick55>
2020-09-29 10:30:26 +0300

    added database tests

--- src/test/java/com/derrick/persistence/dao/JdbcOfficerDAOTest.java
+++ src/test/java/com/derrick/persistence/dao/JdbcOfficerDAOTest.java
@@ -1,32 +1,84 @@
 package com.derrick.persistence.dao;
 
+import com.derrick.persistence.entities.Officer;
+import com.derrick.persistence.entities.Rank;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.*;
 
-class JdbcOfficerDAOTest {
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+//Initialize a database using JPA https://docs.spring.io/spring-boot/docs/1.1.0.M1/reference/html/howto-database-initialization.html
+@TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=validate"
+})
+@Transactional
+public class JdbcOfficerDAOTest {
+    @Qualifier("jdbcOfficerDAO")
+    @Autowired
+    private OfficerDAO dao;
 
     @Test
-    void save() {
+    public void save() {
+        Officer officer = new Officer(Rank.LIEUTENANT, "Nyota", "Uhuru");
+        officer = dao.save(officer);
+        assertNotNull(officer.getId());
     }
 
     @Test
-    void findById() {
+    public void findByIdThatExists() {
+        Optional<Officer> officer = dao.findById(1);
+        assertTrue(officer.isPresent());
+        assertEquals(1, officer.get().getId().intValue());
     }
 
     @Test
-    void findAll() {
+    public void findByIdThatDoesNotExist() {
+        Optional<Officer> officer = dao.findById(999);
+        assertFalse(officer.isPresent());
     }
 
     @Test
-    void count() {
+    public void count() {
+        assertEquals(5, dao.count());
     }
 
     @Test
-    void delete() {
+    public void findAll() {
+        List<String> dbNames = dao.findAll().stream()
+                .map(Officer::getLast)
+                .collect(Collectors.toList());
+        assertThat(dbNames, containsInAnyOrder("Kirk", "Picard", "Sisko", "Janeway", "Archer"));
     }
 
     @Test
-    void existsById() {
+    public void delete() {
+        IntStream.rangeClosed(1, 5)
+                .forEach(id -> {
+                    Optional<Officer> officer = dao.findById(id);
+                    assertTrue(officer.isPresent());
+                    dao.delete(officer.get());
+                });
+        assertEquals(0, dao.count());
+    }
+
+    @Test
+    public void existsById() {
+        IntStream.rangeClosed(1, 5)
+                .forEach(id -> assertTrue(dao.existsById(id)));
     }
 }
